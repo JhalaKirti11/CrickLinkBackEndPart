e62a3d81ee869bb09e8377c4a4519dc3702ff157
Kirti <kirtijhala1110@gmail.com>
2025-02-09 19:08:35 +0530

    no chang

--- model/groupChat.model.js
+++ model/groupChat.model.js
@@ -31,7 +31,7 @@ const group = new mongoose.Schema({
         required: true,
         unique: true
     },
-    c: [{
+    members: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'user'
     }],
