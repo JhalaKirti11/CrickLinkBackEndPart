b2f3b7fb797d8447eb5e3aaea8aab0b635b11fdb
Kirti11 <kirtijhala1110@gmail.com>
2025-04-08 22:00:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,45 +1,3 @@
-// import express from "express";
-// import mongoose from "mongoose"
-// import bodyParser from "body-parser";
-// import cors from "cors";
-
-
-// import UserRouter from "./routes/user.route.js";
-// import TeamRouter from "./routes/Team.route.js";
-// import PlayerRouter from "./routes/player.route.js";
-// import MatchRouter from "./routes/match.route.js";
-// import TournamentRouter from "./routes/tournament.route.js";
-
-// const app = express();
-// app.use(cors());
-
-// mongoose.connect("mongodb://localhost:27017/cricklink")
-//   .then(() => {
-//     console.log("Database connected...");
-//     app.use(bodyParser.json());
-//     app.use(bodyParser.urlencoded({ extended: true }));
-
-//     // app.get("/",(req,res)=>{
-//     //   // console.log("hey")
-//     //   res.end("Done")
-//     // })
-//     app.use("/user", UserRouter);
-//     app.use("/Team", TeamRouter);
-//     app.use("/Tournament", TournamentRouter);
-//     app.use("/match", MatchRouter)
-//     app.use("/player", PlayerRouter);
-
-//     app.listen(3001, () => {
-//       console.log("Server Started....");
-//     });
-
-//   }).catch(err => {
-//     console.log(err);
-//   })
-
-
-//====================================================================
-
 import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
@@ -51,10 +9,7 @@ import TournamentRouter from "./routes/tournament.route.js";
 import PlayerRouter from "./routes/player.route.js";
 import MatchRouter from "./routes/match.route.js";
 import authRoutes from "./routes/authRoutes.js";
-
-
 import groupChatRouter from "./routes/groupChatRouter.js";
-
 
 import path from "path"
 import {fileURLToPath} from "url"
