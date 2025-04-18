a1e004812c077cb4e55ee46366f3ffe72b776df1
Kirti11 <kirtijhala1110@gmail.com>
2025-04-18 22:49:57 +0530

    Update authController.js

--- controller/authController.js
+++ controller/authController.js
@@ -7,17 +7,14 @@ export const googleSignIn = async (req, res) => {
         let user = await User.findOne({ email });
 
         if (!user) {
-            // Naya user create karo bina password ke
             user = new User({
                 email,
                 name,
-                googleId: googleToken, // Google ka unique ID store karo
+                googleId: googleToken,
                 role: "player"
             });
             await user.save();
         }
-
-        // Login successful response
         res.status(200).json({ user, message: "Google Sign-In Successful" });
     } catch (error) {
         console.error("Google Login Error:", error);
